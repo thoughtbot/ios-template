948802a88463000249994039bd0acc85f96745de
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-09-10 13:27:07 -0500

    Mark classes as `final`

--- Sources/Controllers/AppDelegate.swift
+++ Sources/Controllers/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 }
